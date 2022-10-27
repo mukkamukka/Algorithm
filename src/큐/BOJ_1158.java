c6be05fb7910038deeead1f464acd4022235d622
mukkamukka <rlaehdrjs4605@gmail.com>
2022-10-27 21:37:59 +0900

    Algorithm

---
+++ src/큐/BOJ_1158.java
@@ -0,0 +1,42 @@
+package 큐;
+
+/* https://www.acmicpc.net/problem/1158 */
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.add(i + 1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (queue.size() == 1) {
+                sb.append(queue.poll() + ">");
+                break;
+            }
+            for (int j = 0; j < m; j++) {
+                if (j == m - 1) {
+                    sb.append(queue.poll() + ", ");
+                } else {
+                    queue.add(queue.poll());
+                }
+            }
+        }
+
+        bw.write("<" + sb.toString());
+        bw.close();
+    }
+}
