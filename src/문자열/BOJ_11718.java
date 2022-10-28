15b3eca5b402b452ecf3b86d0c397d77d44cb063
mukkamukka <rlaehdrjs4605@gmail.com>
2022-10-28 21:18:21 +0900

    Algorithm

---
+++ src/문자열/BOJ_11718.java
@@ -0,0 +1,17 @@
+package 문자열;
+
+/* https://www.acmicpc.net/problem/11718 */
+
+import java.io.*;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
